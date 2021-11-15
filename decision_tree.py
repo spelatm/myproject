5e70644e37fd76c41a380a256f03191669d3248f
spelatm <914386332@qq.com>
2021-11-15 20:36:45 +0800

    决策树相关的机器学习

--- decision_tree.py
+++ decision_tree.py
@@ -14,9 +14,10 @@ def loadData():
     data = []
     f = open('lenses.txt')
     for line in f.readlines():
-        line_data = line.strip().split()
+        line_data = line.strip().split('\t')
         data.append(line_data)
-    return data
+    labels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    return data, labels
 
 def calshannonent(data):
     nument = len(data)
@@ -83,7 +84,7 @@ def choosebestfeaturetosplit(data):
             prob = len(subDataset) / float(len(data))
             newEntropy += prob * calshannonent(subDataset)
         infogain = baseEntropy - newEntropy
-        if infogain > bestinfogain :
+        if infogain > bestinfogain:
             bestinfogain = infogain
             bestfeatures = i
     return bestfeatures
@@ -92,45 +93,84 @@ def majorityCnt(classList):
     classCount = {}
     for vote in classList:
         if vote not in classCount.keys():
-            classCount = 0
-        classCount += 1
+            classCount[vote] = 0
+        classCount[vote] += 1
     sortedClasCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClasCount[0][0]
 
 def createTree(data, labels):
     classList = [example[-1] for example in data]
     if classList.count(classList[0] == len(classList)):
-        return  classList[0]
-    if len(data[0] == 1):
+        return classList[0]
+    if len(data[0]) == 1:
         return majorityCnt(classList)
+    if len(labels) == 0:
+        return
     bestFeat = choosebestfeaturetosplit(data)
     bestFeatLabel = labels[bestFeat]
-    myTree = {bestFeatLabel:{}}
+    myTree = {bestFeatLabel: {}}
     del(labels[bestFeat])
     featValues = [example[bestFeat] for example in data]
-    uniqueVals = set (featValues)
+    uniqueVals = set(featValues)
     for value in uniqueVals:
         subLabels = labels[:]
         myTree[bestFeatLabel][value] = createTree(splitdata(data, bestFeat, value), subLabels)
 
     return myTree
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
-
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
     createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
                             xytext=centerPt, textcoords='axes fraction',
                             va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
 
+# def createPlot():
+#     fig = plt.figure(1, facecolor='white')
+#     fig.clf()
+#     createPlot.ax1 = plt.subplot(111, frameon=False)
+#     plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+#     plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+#     plt.show()
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[0])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numleafs = getNumleafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.x0ff + (1.0 + float(numleafs))/2.0/plotTree.totalW,
+               plotTree.y0ff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.y0ff = plotTree.y0ff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ =='dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.x0ff = plotTree.x0ff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.x0ff, plotTree.y0ff),
+                     cntrPt, leafNode)
+            plotMidText((plotTree.x0ff, plotTree.y0ff), cntrPt, str(key))
+    plotTree.y0ff = plotTree.y0ff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumleafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.x0ff = -0.5/plotTree.totalW
+    plotTree.y0ff = 1.0
+    plotTree(inTree, (0.5,1.0), '')
+    plt.show()
+
 def getNumleafs(myTree):
     numleafs = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == 'dict':
@@ -142,7 +182,7 @@ def getNumleafs(myTree):
 
 def getTreeDepth(myTree):
     maxDepth = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == 'dict':
@@ -157,4 +197,13 @@ def retrieveTree(i):
     listoftree = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}]
 
-    return  listoftree[i]
+    return listoftree[i]
+
+if __name__ == '__main__':
+    data, labels = loadData()
+    shannonent = calshannonent(data)
+    gini = calgini(data)
+    print("该数据集的信息熵为:", shannonent,
+          "该数据集的基尼系数为:", gini)
+    k = createTree(data, labels)
+    createPlot(k)
